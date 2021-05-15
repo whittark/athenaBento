1d3fb6b4efd6aac4c28fd1c7e8b254c79910f898
Whitney Tarkington <whitney.tarkington@ge.com>
2021-05-15 01:31:13 -0700

    section align test

--- src/components/culture.jsx
+++ src/components/culture.jsx
@@ -9,15 +9,9 @@ export default class Culture extends Component {
           <div className="row mb-5">
           </div>
           <div className="row site-section" id="culture-section">
-            <div className="col-12 text-center" data-aos="fade">
+            <div className="col-md-7 text-center" data-aos="fade">
               <a name="featured" />
               <h2 className="section-title" style={{fontSize: '28px'}}>Payers</h2>
-            </div>
-            <div className="col-12 text-center" data-aos="fade-up" data-aos-delay>
-            </div>
-            <div className="col-lg-2 text-left" data-aos="fade">
-            </div>
-            <div className="col-md-8 text-center" data-aos="fade">
               <p>As payers, you are champions of preventative care. Accessible, nutritious meals result in lower healthcare costs for everyone.</p>
               <p><b>Sign up</b> to learn more or <b>Sign In</b> to access your program dashboard and track subsidies for participation.</p>
             </div>
--- src/components/cybersecurity.jsx
+++ src/components/cybersecurity.jsx
@@ -6,26 +6,19 @@ export default class CyberSecurity extends Component {
       <div>
       <section className="site-section bg-light" id="cybersecurity-section">
         <div className="container">
-          <div className="row mb-5">
-          </div>
-          <div className="row site-section" id="culture-section">
-            <div className="col-12 text-center" data-aos="fade">
+          <div className="row mb-5 justify-content-center">
+          <div className="row site-section" id="cybersecurity-section">
+            <div className="col-md-7 text-center" data-aos="fade">
               <a name="featured" />
               <h2 className="section-title" style={{fontSize: '28px'}}>Partners</h2>
-            </div>
-            <div className="col-12 text-center" data-aos="fade-up" data-aos-delay>
-            </div>
-            <div className="col-lg-2 text-left" data-aos="fade">
-            </div>
-            <div className="col-md-8 text-center" data-aos="fade">
               <p>If you are a food delivery service or grocer, you are perfectly poised to help people live healthier lives. Imagine leading the charge while growing your customer base AND collecting subsidies for your participation.</p>
               <p><b>Sign up</b> to learn more or <b>Sign In</b> to  view customer participation or track reimbursement subsidies.</p>
-              <div className="col-12 text-center" data-aos="fade">
-                <img src="images/cyber_security-sm.jpg" alt="Image" /> 
-              </div>
+              <img src="images/cyber_security-sm.jpg" alt="Image" /> 
             </div>
           </div>
-        </div></section>
+        </div>
+        </div>
+        </section>
       <section>
         <div className="row site-section" id="cybersecurity-section">
         </div>
