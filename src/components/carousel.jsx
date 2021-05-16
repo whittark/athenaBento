5deb3a76c31b1036343eb8827615a9c898f34aaa
Whitney Tarkington <whitney.tarkington@ge.com>
2021-05-15 18:47:59 -0700

    Edited carousel text and provider + payer sections for responsive output

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -25,7 +25,7 @@ export default class Carousel extends Component {
                 <a href="#" className="custom-prev"><span><span className="icon-keyboard_backspace" /></span></a><a href="#" className="custom-next"><span><span className="icon-keyboard_backspace" /></span></a>
               </div>
             </div>
-            <div className="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay={100}>
+            <div className="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay={300}>
               <div className="owl-carousel slide-one-item-alt-text">
                 <div>
                   <h2 className="section-title mb-3">Healthcare Providers. </h2>
@@ -34,22 +34,22 @@ export default class Carousel extends Component {
                 </div>
                 <div>
                   <h2 className="section-title mb-3">Nutritionists</h2>
-                  <p className="lead">Nutritionists submit healthy menu items and grocery lists in the bento portal. These appear on grocery and menu sites as discounted options for clients (healthy eaters).</p>
+                  <p className="lead">Nutritionists submit healthy menu items and grocery lists, which appear as discounted options for healthy eaters on partner sites.</p>
                   <p><a href="#bigdata-section" className="btn btn-primary mr-2 mb-2">Learn more about bento nutritionists</a></p>
                 </div>
                 <div>
                   <h2 className="section-title mb-3">Payers</h2>
-                  <p className="lead">Insurers help subsidize the program to promote community wellness, which results in fewer claims. Payers also receive government funds for participating.</p>
+                  <p className="lead">Insurers help subsidize the program to promote community wellness, which results in fewer claims. Payers receive government funding for participation.</p>
                   <p><a href="#culture-section" className="btn btn-primary mr-2 mb-2">Learn more about bento payers</a></p>
                 </div>
                 <div>
                   <h2 className="section-title mb-3">Healthy Eaters</h2>
-                  <p className="lead">Clients (healthy eaters) receive a questionnaire to fine-tune food preferences. They then access partner sites to shop grocery or menu items selected for them. If recommended items are seleced, they get nutritionally tailored foods at a reduced cost.</p>
+                  <p className="lead">Bento's healthy eaters receive a questionnaire to fine-tune food preferences. They then select from discounted, wholesome options indicated on partner sites.</p>
                   <p><a href="#feature-section" className="btn btn-primary mr-2 mb-2">Learn more about healthy eaters</a></p>
                 </div>
                 <div>
                   <h2 className="section-title mb-3">Partners</h2>
-                  <p className="lead">Our partners are participating grocers, restaurateurs, and food delivery services that can expand their customer base while receving a subsidy for discounted healthy offerings.</p>
+                  <p className="lead">Participating grocers, restaurateurs, and food delivery services expand their customer base while receiving a subsidy for discounting healthy options.</p>
                   <p><a href="#cybersecurity-section" className="btn btn-primary mr-2 mb-2">Learn about bento partners</a></p>
                 </div>
               </div>
